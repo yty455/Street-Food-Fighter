d0260fe9c77e1bc07559624cb5e2cb05dc450455
0214kbs <0214kbs@naver.com>
2023-10-24 11:31:43 +0900

    Style: 프로젝트에 사용할 색상 Theme 적용

--- frontend/src/styles/DefaultTheme.ts
+++ frontend/src/styles/DefaultTheme.ts
@@ -2,25 +2,16 @@ import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
   colors: {
-    main: '#4790FC',
-    white: '#FAFAFA',
-    black: '#363636',
-    lightgray: '#E4E7EC', // disable에 쓰이는 color
-    gray: 'rgba(0, 0, 0, 0.50)', // 진한 회색 글씨
+    main: '#FFCC33',
+    light: '#FFF9E5',
+    white: '#FFFFFE',
+    black: '#1A1300',
+    lightgray: '#EFEFEB',
+    gray: '807E77',
   },
   fonts: {
     bold: 'BoldFont',
     air: 'AirFont',
-  },
-  fontSizes: {
-    xsmall: '12px',
-    small: '16px',
-    medium: '20px',
-    mediumlarge: '26px',
-    large: '32px',
-    xlarge: '40px',
-    xxlarge: '48px',
-    xxxlarge: '60px',
   },
 };
 export default theme;
