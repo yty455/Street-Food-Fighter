6fd3d8733745a7294420647fffe872464981020d
0214kbs <0214kbs@naver.com>
2023-10-24 11:20:38 +0900

    Style: 글씨체 적용

--- frontend/src/styles/DefaultTheme.ts
+++ frontend/src/styles/DefaultTheme.ts
@@ -1,29 +1,26 @@
-import { DefaultTheme } from "styled-components";
+import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
   colors: {
-    main: "#4790FC",
-    white: "#FAFAFA",
-    black: "#363636",
-    lightgray: "#E4E7EC", // disable에 쓰이는 color
-    gray: "rgba(0, 0, 0, 0.50)", // 진한 회색 글씨
+    main: '#4790FC',
+    white: '#FAFAFA',
+    black: '#363636',
+    lightgray: '#E4E7EC', // disable에 쓰이는 color
+    gray: 'rgba(0, 0, 0, 0.50)', // 진한 회색 글씨
   },
   fonts: {
-    HangeulFontMedium: "HangeulFontMedium",
-    HangeulFontRegular: "HangeulFontRegular",
-    HangeulFontSemiBold: "HangeulFontSemiBold",
-    EnglishFontBold: "EnglishFontBold",
-    EnglishFontLight: "EnglishFontLight",
+    bold: 'BoldFont',
+    air: 'AirFont',
   },
   fontSizes: {
-    xsmall: "12px",
-    small: "16px",
-    medium: "20px",
-    mediumlarge: "26px",
-    large: "32px",
-    xlarge: "40px",
-    xxlarge: "48px",
-    xxxlarge: "60px",
+    xsmall: '12px',
+    small: '16px',
+    medium: '20px',
+    mediumlarge: '26px',
+    large: '32px',
+    xlarge: '40px',
+    xxlarge: '48px',
+    xxxlarge: '60px',
   },
 };
 export default theme;
