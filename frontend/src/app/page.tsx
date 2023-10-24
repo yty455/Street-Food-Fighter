f5daf054b3e2327b4f3131c76a108f9e2f5c3a0b
0214kbs <0214kbs@naver.com>
2023-10-24 15:29:44 +0900

    Feat: 메인페이지 생성 - 랜딩페이지에서 메인페이지로 자동 이동 - 이동시 애니메이션 적용

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,41 @@
 'use client';
+import { useState, useEffect } from 'react';
+import MainPage from '@/pagecomponents/main';
 import LandingPage from '@/pagecomponents/landing';
+import styled from 'styled-components';
+
+const FadeOut = styled.div<{ fade: boolean }>`
+  opacity: ${({ fade }) => (fade ? 0 : 1)};
+  transition: opacity 1s;
+`;
 
 export default function Home() {
-  return <LandingPage />;
+  const [showLanding, setShowLanding] = useState(true);
+  const [fadeLanding, setFadeLanding] = useState(false);
+
+  useEffect(() => {
+    const fadeTimer = setTimeout(() => {
+      setFadeLanding(true);
+    }, 400);
+
+    const showTimer = setTimeout(() => {
+      setShowLanding(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(fadeTimer);
+      clearTimeout(showTimer);
+    };
+  }, []);
+
+  return (
+    <>
+      {showLanding && (
+        <FadeOut fade={fadeLanding}>
+          <LandingPage />
+        </FadeOut>
+      )}
+      {!showLanding && <MainPage />}
+    </>
+  );
 }
