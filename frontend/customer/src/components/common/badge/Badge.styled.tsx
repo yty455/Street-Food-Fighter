32fd87064d39c2e5df59db634855dbeef68b13fc
0214kbs <0214kbs@naver.com>
2023-10-25 15:24:07 +0900

    Feat: Badge 공용 컴포넌트 - 컴포넌트 생성 및 디자인

---
+++ frontend/customer/src/components/common/badge/Badge.styled.tsx
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+const BadgeBox = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const main = props.theme.colors.main;
+    return css`
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+
+      padding: 5px 10px;
+      background-color: ${main};
+      border-radius: 4px;
+      font-size: 12px;
+    `;
+  }};
+`;
+
+export { BadgeBox };
