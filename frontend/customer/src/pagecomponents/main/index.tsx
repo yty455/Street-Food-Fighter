e678a5b89d538b24451cd4769fd4258ca270d1c5
0214kbs <0214kbs@naver.com>
2023-10-25 12:07:29 +0900

    Feat: 카카오맵 api 연동 - 카카오맵 api 연동 - 맵 전체 화면 띄우기 - navbar 가려지는 문제 해결

--- frontend/customer/src/pagecomponents/main/index.tsx
+++ frontend/customer/src/pagecomponents/main/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const [map, setMap] = useState<any>();
+  const [marker, setMarker] = useState<any>();
+
+  useEffect(() => {
+    window.kakao.maps.load(() => {
+      const container = document.getElementById('map');
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+
+      setMap(new window.kakao.maps.Map(container, options));
+      setMarker(new window.kakao.maps.Marker());
+    });
+  }, []);
+  return (
+    <div style={{ height: '95vh' }}>
+      <div id="map" style={{ width: '100%', height: '100%' }}></div>
+    </div>
+  );
 };
 
 export default MainPage;
