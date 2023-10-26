cb780b63a2b2f1b75edd94ff0b45268f2dcff0dd
0214kbs <0214kbs@naver.com>
2023-10-26 15:54:46 +0900

    Feat: 좌표로 행정구역정보 받기 - 카카오 로컬 api 이용 - top bar의 주소 정보 변경

--- frontend/customer/src/pagecomponents/main/index.tsx
+++ frontend/customer/src/pagecomponents/main/index.tsx
@@ -1,18 +1,26 @@
+import kakaomapApi from '@/apis/kakaoAPI';
 import { Curpos, Filter, Research, Position, StyledTop, Topbar, CardList } from './Main.styled';
 import Card from '@/components/main/card';
 import FilterComponent from '@/components/main/filter';
 import { useRef, useState } from 'react';
-import { Map, MapTypeControl } from 'react-kakao-maps-sdk';
+import { Map } from 'react-kakao-maps-sdk';
 
 const MainPage = () => {
+  const [addressName, setAddressName] = useState('부산시 강서구 녹산동');
   // 중심좌표
+
   const mapRef = useRef<kakao.maps.Map>(null);
-  const handleResearchClick = () => {
+  const handleResearchClick = async () => {
     const map = mapRef.current;
     if (!map) return;
 
     const center = map.getCenter();
-    console.log('중심 좌표:', center.getLat(), center.getLng());
+    const latitude = center.getLat();
+    const longitude = center.getLng();
+
+    // console.log('중심 좌표:', center.getLat(), center.getLng());
+    const address = await kakaomapApi({ latitude, longitude });
+    if (address) setAddressName(address);
   };
 
   // filter
@@ -21,16 +29,16 @@ const MainPage = () => {
 
   return (
     <div style={{ height: '93vh' }}>
-      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '100%', height: '100%' }} ref={mapRef}></Map>
+      <Map center={{ lat: 35.08541957184095, lng: 128.87934499308867 }} style={{ width: '100%', height: '100%' }} ref={mapRef}></Map>
       <StyledTop>
         <Topbar>
           <Filter onClick={toggleFilter}>
             <img src="/images/top/filter.png" style={{ width: '35px' }} />
           </Filter>
 
-          <Position>부산시 강서구 녹산동</Position>
+          <Position>{addressName}</Position>
         </Topbar>
-        <Research onClick={handleResearchClick}>현 위치에서 재검색</Research>
+        <Research onClick={handleResearchClick}>현 지도에서 검색</Research>
       </StyledTop>
       {isFilterVisible && <FilterComponent onClose={toggleFilter} />}
 
