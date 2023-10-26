c94822d0727f8c6e19f807b0a51c880592708580
강동윤 <yty455@naver.com>
2023-10-26 17:59:04 +0900

    init : store-server

---
+++ backend/StoreServer/src/test/java/com/ssafy/ssf/storeserver/StoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssf.storeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
