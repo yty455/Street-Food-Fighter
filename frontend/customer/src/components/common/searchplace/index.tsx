79a3ee11dfb9fbd252adb84eaa22a44d54a9fa5d
0214kbs <0214kbs@naver.com>
2023-10-27 14:34:02 +0900

    Design: 주소검색 디자인

--- frontend/customer/src/components/common/searchplace/index.tsx
+++ frontend/customer/src/components/common/searchplace/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ModalOverlay, SearchTop, TopTitle, SearchBar, SearchText } from './Searchplace.styled';
+import { ModalOverlay, List, SearchTop, TopTitle, SearchBar, SearchText, Place, BoldText, LightText, NoResult } from './Searchplace.styled';
 import kakaosearchApi from '@/apis/kakaoSearchAPI';
 
 type SearchResult = {
@@ -37,24 +37,38 @@ const SearchPlace = ({ onClose }: any) => {
 
   return (
     <ModalOverlay>
-      <SearchTop>
-        <div onClick={onClose}>
-          <img src="/images/top/back.png" style={{ width: '30px' }} />
-        </div>
-        <TopTitle> 주소 검색</TopTitle>
-      </SearchTop>
-      <SearchBar>
-        <img src="/images/orderfunding/search.png" style={{ width: '30px' }} />
-        <SearchText value={query} onChange={(e: any) => setQuery(e.target.value)} placeholder="지번, 도로명, 건물명으로 검색" />
-      </SearchBar>
-      {results.map((result, index) => (
-        <div key={index}>
-          <div>{result.place_name}</div>
-          <div>{result.address_name}</div>
-        </div>
-      ))}
+      <div style={{ padding: '20px' }}>
+        <SearchTop>
+          <div onClick={onClose}>
+            <img src="/images/top/back.png" style={{ width: '30px' }} />
+          </div>
+          <TopTitle> 주소 검색</TopTitle>
+        </SearchTop>
+        <SearchBar>
+          <img src="/images/orderfunding/search.png" style={{ width: '30px' }} />
+          <SearchText value={query} onChange={(e: any) => setQuery(e.target.value)} placeholder="지번, 도로명, 건물명으로 검색" />
+        </SearchBar>
+      </div>
+      <List>
+        {results.length > 0 ? (
+          results.map((result, index) => (
+            <Place key={index}>
+              <BoldText>{result.place_name}</BoldText>
+              <LightText>{result.address_name}</LightText>
+            </Place>
+          ))
+        ) : (
+          <NoResult>
+            <BoldText> 이렇게 검색해보세요 !</BoldText>
+            <div>
+              <LightText> - 도로명 + 건물번호</LightText>
+              <LightText> - 지역명 + 번지</LightText>
+              <LightText> - 건물명, 아파트명</LightText>
+            </div>
+          </NoResult>
+        )}
+      </List>
     </ModalOverlay>
   );
 };
-
 export default SearchPlace;
