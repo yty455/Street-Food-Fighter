25676d98f160ff0e5ff37197c1b93d87a8e62216
0214kbs <0214kbs@naver.com>
2023-10-29 23:10:45 +0900

    Feat: reviewlist 하나씩 ReviewCard 컴포넌트로 보내기

--- frontend/customer/src/components/order/reviewlist/index.tsx
+++ frontend/customer/src/components/order/reviewlist/index.tsx
@@ -1,12 +1,16 @@
 import { vendordata } from '@/temp/vendordata';
 import ReviewCard from '../reviewcard';
+import { BoxContainer } from './Reviewlist.styled';
 
 const Reviewlist = ({ vendorid }: any) => {
   const vendor = vendordata[vendorid];
+  const reviewlist = vendor?.reviewlist || [];
   return (
-    <div>
-      <ReviewCard vendorid={vendorid} />
-    </div>
+    <BoxContainer>
+      {reviewlist.map((review) => (
+        <ReviewCard key={review.reviewid} vendorid={vendorid} reviewid={review.reviewid} />
+      ))}
+    </BoxContainer>
   );
 };
 
