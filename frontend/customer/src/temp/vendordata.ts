79f0ef203bd20c8b41a2e2196e48b9b35667e8aa
0214kbs <0214kbs@naver.com>
2023-10-29 19:12:42 +0900

    Feat: 가게 API에 대한 mock data 수정

--- frontend/customer/src/temp/vendordata.ts
+++ frontend/customer/src/temp/vendordata.ts
@@ -7,6 +7,30 @@ export const vendordata = [
     category: 1,
     review: 4.8,
     phone: '010-1111-1111',
+    introduction: '녹산 호떡 트럭 소개입니다.',
+    starttime: '08:00',
+    endtime: '17:00',
+    loc: '갑을 녹산 병원 앞',
+    notice: '녹산 호떡 트럭을 찾아주셔서 감사합니다.더욱 맛있는 호떡을 만들어 드리겠습니다.',
+    menulist: [
+      {
+        id: 1,
+        name: '호호떡',
+        options: [
+          {
+            id: 1,
+            name: '꿀 빼주세요',
+            price: 500,
+          },
+          {
+            id: 2,
+            name: '꿀 더주세요',
+            price: 500,
+          },
+        ],
+        price: 8000,
+      },
+    ],
   },
   {
     id: 2,
@@ -16,5 +40,46 @@ export const vendordata = [
     category: 2,
     review: 4.3,
     phone: '010-1234-1111',
+    introduction: '세계 최강 맛있는 붕어빵집 세계 최강 붕어빵 입니다. 색다른 붕어빵들을 먹어보세요.',
+    starttime: '08:00',
+    endtime: '17:00',
+    loc: '녹산동 다이소 앞',
+    notice: '세계 최강 붕어빵을 찾아주셔서 감사합니다.더욱 맛있는 붕어빵을 만들어 드리겠습니다.',
+    menulist: [
+      {
+        id: 1,
+        name: '세계 최강 팥 붕어빵',
+        options: [
+          {
+            id: 1,
+            name: '초코크림 빼주세요',
+            price: 500,
+          },
+          {
+            id: 2,
+            name: '초코크림 더주세요',
+            price: 500,
+          },
+        ],
+        price: 8000,
+      },
+      {
+        id: 2,
+        name: '세계 최강 슈크림 붕어빵',
+        options: [
+          {
+            id: 1,
+            name: '슈크림크림 빼주세요',
+            price: 500,
+          },
+          {
+            id: 2,
+            name: '슈크림크림 더주세요',
+            price: 500,
+          },
+        ],
+        price: 9000,
+      },
+    ],
   },
 ];
