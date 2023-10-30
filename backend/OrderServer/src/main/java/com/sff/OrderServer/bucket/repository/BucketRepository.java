b9e1a0bf10f8f2fefd29c79dd64bf762bd494ee0
deerKBS <deer980119@gmail.com>
2023-10-30 17:58:37 +0900

    Feat : Bucket 상품 바구니 생성 및 상품 목록 추가 API 추가

---
+++ backend/OrderServer/src/main/java/com/sff/OrderServer/bucket/repository/BucketRepository.java
@@ -0,0 +1,11 @@
+package com.sff.OrderServer.bucket.repository;
+
+import com.sff.OrderServer.bucket.entity.Bucket;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BucketRepository extends JpaRepository<Bucket, Long> {
+    Optional<Bucket> findByUserIdAndPaymentStateFalse(Long userId);
+}
