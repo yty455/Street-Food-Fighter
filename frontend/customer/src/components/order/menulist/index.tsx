d665e1edacc98fd94b126cf1bebd1ca791045f03
0214kbs <0214kbs@naver.com>
2023-10-30 14:53:24 +0900

    Feat: 체크박스&수량을 체크한 항목이 있는경우, 담기 버튼

--- frontend/customer/src/components/order/menulist/index.tsx
+++ frontend/customer/src/components/order/menulist/index.tsx
@@ -1,16 +1,18 @@
 import { vendordata } from '@/temp/vendordata';
 import MenuCard from '../menucard';
-import { BoxContainer } from './Menulist.styled';
+import { BoxContainer, Putin } from './Menulist.styled';
+import useOrderStore from '@/stores/orderStore';
 
 const Menulist = ({ vendorid }: any) => {
   const vendor = vendordata[vendorid];
   const menulist = vendor?.menulist || [];
-
+  const { order } = useOrderStore();
   return (
     <BoxContainer>
       {menulist.map((menu) => (
         <MenuCard key={menu.id} vendorid={vendorid} menuid={menu.id} />
       ))}
+      {order.length != 0 && <Putin> 담 기 </Putin>}
     </BoxContainer>
   );
 };
