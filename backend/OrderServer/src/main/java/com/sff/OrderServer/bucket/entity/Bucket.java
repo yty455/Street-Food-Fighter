f81c59145c0fb21203ccf18e09eeab0f73a5cc44
deerKBS <deer980119@gmail.com>
2023-10-31 15:30:04 +0900

    Feat : 결제 상태 변경 메서드 추가

--- backend/OrderServer/src/main/java/com/sff/OrderServer/bucket/entity/Bucket.java
+++ backend/OrderServer/src/main/java/com/sff/OrderServer/bucket/entity/Bucket.java
@@ -1,11 +1,13 @@
 package com.sff.OrderServer.bucket.entity;
 
 import com.sff.OrderServer.bucket.dto.Item;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,5 +37,9 @@ public class Bucket {
                 .userId(userId)
                 .totalPrice(totalPrice)
                 .build();
+    }
+
+    public void updateState(){
+        this.paymentState=true;
     }
 }
