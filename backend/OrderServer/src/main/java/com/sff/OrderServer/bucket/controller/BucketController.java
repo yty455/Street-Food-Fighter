7d56da447f959baa2afe39de90900a50aa1561f1
deerKBS <deer980119@gmail.com>
2023-10-31 15:30:26 +0900

    Feat : 결제 상태 변경 API 추가

--- backend/OrderServer/src/main/java/com/sff/OrderServer/bucket/controller/BucketController.java
+++ backend/OrderServer/src/main/java/com/sff/OrderServer/bucket/controller/BucketController.java
@@ -7,8 +7,10 @@ import com.sff.OrderServer.utils.ApiUtils;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,9 +20,13 @@ public class BucketController {
     private final BucketService bucketService;
 
     @PostMapping("/api/order-server/user/buckets")
-    public ApiResult<?> createbucket(@RequestHeader("userId") Long userId, @RequestBody List<Item> items){
-        bucketService.createBucket(userId, items);
-        return ApiUtils.success("바구니 생성 및 저장 성공");
+    public ApiResult<?> createBucket(@RequestHeader("userId") Long userId, @RequestBody List<Item> items){
+        return ApiUtils.success(bucketService.createBucket(userId, items));
     }
 
+    @PutMapping("/api/order-server/user/buckets")
+    public ApiResult<?> updateBucket(@RequestHeader("userId") Long userId, @RequestParam Long bucketId){
+        bucketService.updateBucketPaymentState(userId, bucketId);
+        return ApiUtils.success("bucket 결제 상태로 변경 완료");
+    }
 }
