071337a1fa9c447425e06f77b7c04db1fba96326
김성인 <kimsungin5@gmail.com>
2023-11-01 09:49:09 +0900

    Feat: 손님 JWT 재발급 요청

--- backend/UserServer/src/main/java/com/sff/userserver/domain/member/controller/JwtController.java
+++ backend/UserServer/src/main/java/com/sff/userserver/domain/member/controller/JwtController.java
@@ -1,16 +1,11 @@
 package com.sff.userserver.domain.member.controller;
 
-import com.sff.userserver.domain.member.dto.JwtRequest;
 import com.sff.userserver.global.jwt.service.JwtService;
 import com.sff.userserver.global.utils.ApiResult;
 import com.sff.userserver.global.utils.ApiUtils;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class JwtController {
     private final JwtService jwtService;
 
-    @PostMapping("/jwt")
-    public ApiResult<?> reissueJwt(@Valid @RequestBody JwtRequest jwtRequest, HttpServletResponse response) {
-        jwtService.findMemberAndUpdateJwt(jwtRequest.getRefreshToken(), response);
+    @GetMapping("/jwt")
+    public ApiResult<?> reissueJwt(@RequestParam("refresh-token") String refreshToken, HttpServletResponse response) {
+        jwtService.findMemberAndUpdateJwt(refreshToken, response);
 
         return ApiUtils.success("JWT Access & Refresh 재발급 성공");
     }
