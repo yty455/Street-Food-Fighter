1347b69538a5f9cdac762a6cd75f192e872ff313
0214kbs <0214kbs@naver.com>
2023-11-01 12:21:07 +0900

    Feat: 알림메시지 카드 컴포넌트 - 컴포넌트 분리 - 레이아웃 설계

---
+++ frontend/customer/src/components/alert/alertcard/index.tsx
@@ -0,0 +1,15 @@
+const AlertCard = (alert: any) => {
+  return (
+    <div>
+      <div>
+        <div>알림 타이틀</div>
+        <div>알림 이미지</div>
+      </div>
+
+      <div>알림 내용</div>
+      <div>알림 경과 시간</div>
+    </div>
+  );
+};
+
+export default AlertCard;
