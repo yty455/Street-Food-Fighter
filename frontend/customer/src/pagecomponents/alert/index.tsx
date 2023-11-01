549514e889b6f9912589abf186b724244b837827
0214kbs <0214kbs@naver.com>
2023-11-01 11:09:21 +0900

    Feat: 알림 페이지 레이아웃

---
+++ frontend/customer/src/pagecomponents/alert/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AlertPage = () => {
+  return (
+    <div style={{ height: '93vh' }}>
+      <div>
+        <div>알림 센터</div>
+        <img src="/images/top/alert.png" style={{ width: '30px' }} />
+      </div>
+      <div>
+        <div> 오늘 알림 리스트 </div>
+        <div> 이전 알림 보기 </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlertPage;
