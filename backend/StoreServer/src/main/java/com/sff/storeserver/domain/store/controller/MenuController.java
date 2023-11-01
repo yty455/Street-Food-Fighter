dc26440c1ff5d9b077bf80a6d212d84cf8fc4b18
slbin <smpts00@naver.com>
2023-11-01 11:33:33 +0900

    Feat: 가게 메뉴 추가 , 조회 API 구현

--- backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/controller/MenuController.java
+++ backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/controller/MenuController.java
@@ -3,13 +3,13 @@ package com.sff.storeserver.domain.store.controller;
 import com.sff.storeserver.common.utils.ApiResult;
 import com.sff.storeserver.common.utils.ApiUtils;
 import com.sff.storeserver.domain.store.dto.MenuInfo;
+import com.sff.storeserver.domain.store.dto.MenuInfoResponse;
 import com.sff.storeserver.domain.store.service.MenuService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/store-service")
@@ -17,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MenuController {
     private final MenuService menuService;
 
-    @PostMapping("/menus")
-    public ApiResult<String> createMenus(@Valid @RequestBody MenuInfo menuInfo) {
-        menuService.createMenus(menuInfo);
-        return ApiUtils.success("성공");
+    @PostMapping("/menus/{storeId}")
+    public ApiResult<String> createMenus(@PathVariable Long storeId, @Valid @RequestBody MenuInfo menuInfo) {
+        menuService.createMenus(menuInfo, storeId);
+        return ApiUtils.success("상품 추가를 성공했습니다.");
     }
+
+    @GetMapping("/menus/{storeId}")
+    public ApiResult<List<MenuInfoResponse>> getMenus(@PathVariable Long storeId) {
+        return ApiUtils.success(menuService.getMenus(storeId));
+    }
+
 }
--- backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/service/MenuService.java
+++ backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/service/MenuService.java
@@ -1,20 +1,43 @@
 package com.sff.storeserver.domain.store.service;
 
+import com.sff.storeserver.common.error.code.StoreError;
+import com.sff.storeserver.common.error.type.BaseException;
 import com.sff.storeserver.domain.store.dto.MenuInfo;
+import com.sff.storeserver.domain.store.dto.MenuInfoResponse;
+import com.sff.storeserver.domain.store.entity.Menu;
+import com.sff.storeserver.domain.store.entity.Store;
 import com.sff.storeserver.domain.store.repository.MenuRepository;
+import com.sff.storeserver.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class MenuService {
     private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
 
-    public void createMenus(MenuInfo menuInfo) {
+    public void createMenus(MenuInfo menuInfo, Long storeId) {
+        Store store = storeRepository.findById(storeId).orElseThrow(() ->
+                new BaseException(StoreError.NOT_FOUND_STORE)
+        );
+        Menu menu = menuInfo.toEntity();
+        menu.getOptions().forEach(option -> {
+            log.info(String.valueOf(option.getPrice()));
+            log.info(option.getMenu().getName());
+        });
+        menu.addStore(store);
+        menuRepository.save(menu);
+    }
 
+    public List<MenuInfoResponse> getMenus(Long storeId) {
+        List<Menu> menus = menuRepository.findByStoreId(storeId);
+        return menus.stream().map(MenuInfoResponse::fromEntity).toList();
     }
 }
