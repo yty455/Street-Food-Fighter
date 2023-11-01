cf722ca49ce996642c3c5eb6cd79cd55795a0439
0214kbs <0214kbs@naver.com>
2023-11-01 22:13:36 +0900

    Feat: Level type

---
+++ frontend/customer/src/assets/level.ts
@@ -0,0 +1,20 @@
+export const levels = {
+  champion: {
+    image: '/images/level/champion.png',
+    label: '챔피언',
+  },
+  heavy: {
+    image: '/images/level/heavy.png',
+    label: '헤비',
+  },
+  light: {
+    image: '/images/level/light.png',
+    label: '라이트',
+  },
+  middle: {
+    image: '/images/level/middle.png',
+    label: '미들',
+  },
+};
+
+export default levels;
