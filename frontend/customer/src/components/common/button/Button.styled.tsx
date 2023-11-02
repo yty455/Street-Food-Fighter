71f251e982b1b6f85384970662c312eaa47e22f9
0214kbs <0214kbs@naver.com>
2023-11-02 11:17:56 +0900

    Style: 버튼 스타일 수정

--- frontend/customer/src/components/common/button/Button.styled.tsx
+++ frontend/customer/src/components/common/button/Button.styled.tsx
@@ -5,6 +5,7 @@ const StyledButton = styled.div.attrs<any>((props) => ({}))`
     const main = props.theme.colors.main;
     const black = props.theme.colors.black;
     const color = props.color ? props.theme.colors[props.color] : main;
+    const size = props.fontsize || '16px';
     return css`
       padding: 5px 15px;
       border: none;
@@ -12,6 +13,14 @@ const StyledButton = styled.div.attrs<any>((props) => ({}))`
       background-color: ${color};
       color: ${black};
       text-align: center;
+
+      //
+      height: 100%;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: ${size};
     `;
   }};
 `;
