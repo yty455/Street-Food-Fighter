cca7354e54e3cd812c7172cac37ad7ea933ec77f
0214kbs <0214kbs@naver.com>
2023-11-02 11:41:01 +0900

    Feat: 모달 안의 박스를 클릭하면 닫히지 않게

--- frontend/customer/src/components/mypage/levelmodal/index.tsx
+++ frontend/customer/src/components/mypage/levelmodal/index.tsx
@@ -1,9 +1,13 @@
 import { ModalBackground, ModalContainer } from './Levelmodal.styled';
 
 const LevelModal = ({ toggleModal }: any) => {
+  const handleContainerClick = (e: any) => {
+    e.stopPropagation();
+  };
+
   return (
     <ModalBackground onClick={toggleModal}>
-      <ModalContainer>
+      <ModalContainer onClick={handleContainerClick}>
         <div>파이터 등급</div>
         <div>저번달 주문 횟수를 기준으로 파이터 등급이 정해져요! 등급별로 다양한 혜택을 만나보세요!</div>
         <div>라이트</div>
