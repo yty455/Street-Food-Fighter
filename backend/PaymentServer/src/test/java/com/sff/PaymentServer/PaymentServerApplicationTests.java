36abb8280a5fbb6a7817e4a05aefd3ad440c1731
deerKBS <deer980119@gmail.com>
2023-11-03 17:12:12 +0900

    Feat : 기본 설정 파일들 추가

---
+++ backend/PaymentServer/src/test/java/com/sff/PaymentServer/PaymentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sff.PaymentServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
