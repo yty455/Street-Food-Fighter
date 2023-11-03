90a79dcf8e21abfed8798c230647fd7c0ee23f03
deerKBS <deer980119@gmail.com>
2023-11-03 10:24:01 +0900

    Refactor : 결제 동작에 따라 펀딩 정보 저장 및 펀딩, 바구니 상태 변경 API 수정 - 펀딩 요청 - 펀딩 정보 저장 : 결제 중 - 결제 - 펀딩 정보 수정 : 대기(결제 성공) + 바구니 상태 변경

--- backend/OrderServer/src/main/java/com/sff/OrderServer/error/code/BucketError.java
+++ backend/OrderServer/src/main/java/com/sff/OrderServer/error/code/BucketError.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 
 public enum BucketError implements ServiceError {
-    NON_EXIST_BUCKET_USER(3300, "입력한 유저와 바구니 정보가 존재하지 않습니다.");
+    NON_EXIST_BUCKET_USER(3300, "입력한 유저와 바구니 정보가 존재하지 않습니다."),
+    UPDATE_BUCKET_STATE_ERROR(3301, "바구니 결제 상태 변경에 실패했습니다.")
+    ;
 
     private final int code;
     private final String message;
