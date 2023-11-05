1f6902afff01ea13ca63cb1deb284ea5f022ff1f
deerKBS <deer980119@gmail.com>
2023-11-06 03:33:05 +0900

    Feat : openfeign

--- backend/PaymentServer/src/main/java/com/sff/PaymentServer/PaymentServerApplication.java
+++ backend/PaymentServer/src/main/java/com/sff/PaymentServer/PaymentServerApplication.java
@@ -2,8 +2,10 @@ package com.sff.PaymentServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PaymentServerApplication {
 
 	public static void main(String[] args) {
---
+++ backend/PaymentServer/src/main/java/com/sff/PaymentServer/infra/UserClient.java
@@ -0,0 +1,13 @@
+package com.sff.PaymentServer.infra;
+
+import com.sff.PaymentServer.dto.PurposeCreateRequest;
+import com.sff.PaymentServer.utils.ApiResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@FeignClient(name = "userClient", url = "${spring.data.User_Server}")
+public interface UserClient {
+
+    @PutMapping("/api/user-server/{memberId}/points")
+    ApiResult updateUserPoint(PurposeCreateRequest purposeCreateRequest);
+}
