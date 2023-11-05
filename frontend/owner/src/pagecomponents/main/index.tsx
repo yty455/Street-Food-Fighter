c8fe7b689b54db6da561cf815c75d01410471f2b
0214kbs <0214kbs@naver.com>
2023-11-05 16:45:09 +0900

    Feat: 사장님 메인 레이아웃

---
+++ frontend/owner/src/pagecomponents/main/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <div>
+        <div>영업 시작</div>
+        <div>영업 종료</div>
+      </div>
+      <div>
+        <div>가게 정보 관리</div>
+        <div>상품 관리</div>
+        <div>카테고리 설정</div>
+        <div>깃발 등록</div>
+        <div>주문 목록</div>
+        <div>지역 통계</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
