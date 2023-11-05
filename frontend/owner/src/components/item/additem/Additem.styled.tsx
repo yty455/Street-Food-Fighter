9375e415e7f1ccaf51c6f22fd0a99b448232d46f
0214kbs <0214kbs@naver.com>
2023-11-06 00:56:33 +0900

    Feat: 상품 추가 모달의 메뉴 부분

--- frontend/owner/src/components/item/additem/Additem.styled.tsx
+++ frontend/owner/src/components/item/additem/Additem.styled.tsx
@@ -2,10 +2,12 @@ import styled, { css } from 'styled-components';
 
 const Container = styled.div.attrs<any>((props) => ({}))`
   ${(props) => {
-    const white = props.theme.colors.white;
+    const gray = props.theme.colors.lightgray;
     return css`
       display: flex;
-      background-color: ${white};
+      flex-direction: column;
+
+      background-color: ${gray};
 
       width: 100vw;
       height: 100vh;
@@ -14,5 +16,93 @@ const Container = styled.div.attrs<any>((props) => ({}))`
     `;
   }};
 `;
+const MenuBox = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const white = props.theme.colors.white;
+    return css`
+      background-color: ${white};
 
-export { Container };
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 20px;
+      gap: 25px;
+
+      margin-top: 15px;
+    `;
+  }};
+`;
+
+const MenuContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60vw;
+  gap: 10px;
+`;
+
+const ImageBox = styled.div`
+  position: relative;
+`;
+const MenuImage = styled.img.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const main = props.theme.colors.main;
+    const white = props.theme.colors.white;
+    return css`
+      display: flex;
+      background-color: ${white};
+      align-items: center;
+      justify-content: center;
+      border: 1px solid ${main};
+      border-radius: 14px;
+
+      width: 80px;
+      height: 80px;
+    `;
+  }};
+`;
+
+const Edit = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const gray = props.theme.colors.gray;
+    const lightgray = props.theme.colors.lightgray;
+    const font = props.theme.fonts.air;
+    return css`
+      background-color: ${gray};
+      border-radius: 0px 0px 14px 14px;
+      color: ${lightgray};
+
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      text-align: center;
+      padding: 3px 0px;
+
+      font-family: ${font};
+      font-size: 14px;
+    `;
+  }};
+`;
+const LittleTitle = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const air = props.theme.fonts.air;
+    return css`
+      font-family: ${air};
+      font-size: 14px;
+    `;
+  }};
+`;
+
+const Content = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const air = props.theme.fonts.air;
+    const black = props.theme.colors.black;
+    return css`
+      font-family: ${air};
+      font-size: 18px;
+      width: 100%;
+      text-align: right;
+      border-bottom: 1px solid ${black};
+    `;
+  }};
+`;
+export { ImageBox, Container, MenuBox, Edit, MenuContentBox, MenuImage, LittleTitle, Content };
--- frontend/owner/src/components/item/additem/index.tsx
+++ frontend/owner/src/components/item/additem/index.tsx
@@ -1,11 +1,29 @@
 import Topbar from '@/components/common/topbar';
-import { Container } from './Additem.styled';
+import { ImageBox, Container, Content, Edit, LittleTitle, MenuBox, MenuContentBox, MenuImage } from './Additem.styled';
 
 const AddItem = ({ closeModal }: any) => {
+  const name = '쫀득 쫀득 팥 붕어빵';
+  const price = 10000;
+  const menuUrl = '/images/common/defaultmenuimg.png';
   return (
     <Container>
       <Topbar text="상품 추가" type="close" closeModal={closeModal}></Topbar>
-      <div></div>
+      <MenuBox>
+        <ImageBox>
+          <MenuImage src={menuUrl} />
+          <Edit>Edit</Edit>
+        </ImageBox>
+        <MenuContentBox>
+          <div>
+            <LittleTitle>상품명</LittleTitle>
+            <Content>name</Content>
+          </div>
+          <div>
+            <LittleTitle> 가격</LittleTitle>
+            <Content>{price}원</Content>
+          </div>
+        </MenuContentBox>
+      </MenuBox>
     </Container>
   );
 };
