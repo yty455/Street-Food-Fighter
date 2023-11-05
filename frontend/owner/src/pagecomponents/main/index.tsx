18cbf671fd1e1c032b5c5a395cc4de8d11e72afa
0214kbs <0214kbs@naver.com>
2023-11-05 17:43:24 +0900

    Feat: 메인페이지 완성

--- frontend/owner/src/pagecomponents/main/index.tsx
+++ frontend/owner/src/pagecomponents/main/index.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import { MainContainer, OperButtonList, OperButton, OperText, Menu, MenuList } from './Main.styled';
 
 const MainPage = () => {
   return (
-    <div>
-      <div>
-        <div>영업 시작</div>
-        <div>영업 종료</div>
-      </div>
-      <div>
-        <div>가게 정보 관리</div>
-        <div>상품 관리</div>
-        <div>카테고리 설정</div>
-        <div>깃발 등록</div>
-        <div>주문 목록</div>
-        <div>지역 통계</div>
-      </div>
-    </div>
+    <MainContainer>
+      <OperButtonList>
+        <OperButton type="start">
+          <OperText>영업 시작</OperText>
+        </OperButton>
+        <OperButton>
+          <OperText>영업 종료</OperText>
+        </OperButton>
+      </OperButtonList>
+      <MenuList>
+        <Menu>
+          <img src="/images/common/vendor.png" style={{ width: '75px' }} />
+          <div>가게 정보 관리</div>
+        </Menu>
+        <Menu>
+          <img src="/images/common/item.png" style={{ width: '75px' }} />
+          <div>상품 관리</div>
+        </Menu>
+        <Menu>
+          <img src="/images/common/category.png" style={{ width: '75px' }} />
+          <div>카테고리 설정</div>
+        </Menu>
+        <Menu>
+          <img src="/images/common/flag.png" style={{ width: '75px' }} />
+          <div>깃발 등록</div>
+        </Menu>
+        <Menu>
+          <img src="/images/common/list.png" style={{ width: '75px' }} />
+          <div>주문 목록</div>
+        </Menu>
+        <Menu>
+          <img src="/images/common/graph.png" style={{ width: '75px' }} />
+          <div>지역 통계</div>
+        </Menu>
+      </MenuList>
+    </MainContainer>
   );
 };
 
