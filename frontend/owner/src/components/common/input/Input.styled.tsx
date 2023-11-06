9751a1d801bda83e279eb95e5066a1a72dacdf2d
0214kbs <0214kbs@naver.com>
2023-11-06 10:24:47 +0900

    Feat: Input 공용 컴포넌트 생성

---
+++ frontend/owner/src/components/common/input/Input.styled.tsx
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components';
+
+const InputContainer = styled.div`
+  margin: 10px 0;
+`;
+
+const StyledInput = styled.input.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const white = props.theme.colors.white;
+    const gray = props.theme.colors.gray;
+    const black = props.theme.colors.black;
+    const airfont = props.theme.fonts.air;
+    return css`
+      width: 100%;
+      font-family: ${airfont};
+
+      font-size: 18px;
+      background-color: ${white};
+      box-sizing: border-box;
+
+      border: none;
+      border-bottom: 1px solid ${black};
+
+      &::placeholder {
+        color: ${gray};
+        font-size: 14px;
+      }
+      &:focus {
+        outline: none;
+      }
+    `;
+  }};
+`;
+
+export { InputContainer, StyledInput };
