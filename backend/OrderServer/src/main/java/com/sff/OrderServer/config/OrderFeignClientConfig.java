3d601f9fe8aee20a5f701fbfe2d34e84ef92cec7
deerKBS <deer980119@gmail.com>
2023-11-06 16:55:49 +0900

    Feat : OpenFeign 설정 및 예시 파일 추가

--- backend/OrderServer/src/main/java/com/sff/OrderServer/config/OrderFeignClientConfig.java
+++ backend/OrderServer/src/main/java/com/sff/OrderServer/config/OrderFeignClientConfig.java
@@ -1,8 +1,31 @@
 package com.sff.OrderServer.config;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 public class OrderFeignClientConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                        .getRequestAttributes();
 
+                if (attributes == null) {
+                    return;
+                }
+
+                String userId = attributes.getRequest().getHeader("userId");
+                if (userId != null) {
+                    template.header("userId", userId);
+                }
+            }
+        };
+    }
 }
