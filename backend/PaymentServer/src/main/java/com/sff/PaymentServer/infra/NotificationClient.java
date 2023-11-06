43d04e054a72d085b2cca507cf769d682548840b
deerKBS <deer980119@gmail.com>
2023-11-06 17:41:54 +0900

    Feat : Notification Server 통신 코드 추가 - Dto 추가

--- backend/PaymentServer/src/main/java/com/sff/PaymentServer/infra/NotificationClient.java
+++ backend/PaymentServer/src/main/java/com/sff/PaymentServer/infra/NotificationClient.java
@@ -1,8 +1,11 @@
 package com.sff.PaymentServer.infra;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "notificationClient", url = "${spring.data.Notification_Server}")
 public interface NotificationClient {
+
+
 
 }
