e5345bc618e6a4103159e7acc0d4f8fb544b4c36
0214kbs <0214kbs@naver.com>
2023-11-07 01:05:05 +0900

    Feat: 요일 tab 컴포넌트 생성

---
+++ frontend/owner/src/pagecomponents/flag/Flag.styled.tsx
@@ -0,0 +1,11 @@
+import styled, { css } from 'styled-components';
+
+const TabBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  padding: 0px 10px;
+`;
+
+export { TabBox };
