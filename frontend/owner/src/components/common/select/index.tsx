cbe353bfd7c23dc0096f7773ca4e2635fda41140
0214kbs <0214kbs@naver.com>
2023-11-06 15:48:28 +0900

    Feat: 업태, 카테고리

--- frontend/owner/src/components/common/select/index.tsx
+++ frontend/owner/src/components/common/select/index.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import { ChoiceContainer, Option } from './Select.styled';
-const Select = ({ a, b }: any) => {
+const Select = ({ a, b, onSelect }: any) => {
   const [selectedOption, setSelectedOption] = useState(a);
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    onSelect(option);
+  };
 
   return (
     <ChoiceContainer>
-      <Option isselected={(selectedOption === a).toString()} onClick={() => setSelectedOption(a)}>
+      <Option isselected={(selectedOption === a).toString()} onClick={() => handleSelect(a)}>
         {a}
       </Option>
-      <Option isselected={(selectedOption === b).toString()} onClick={() => setSelectedOption(b)}>
+      <Option isselected={(selectedOption === b).toString()} onClick={() => handleSelect(b)}>
         {b}
       </Option>
     </ChoiceContainer>
--- frontend/owner/src/pagecomponents/category/index.tsx
+++ frontend/owner/src/pagecomponents/category/index.tsx
@@ -1,18 +1,50 @@
+import Select from '@/components/common/select';
 import Topbar from '@/components/common/topbar';
+import { Container, SettingBox, TypeBox, Title, CategoriesContainer, CategoryImage, CategoryName, CategoryItem } from './Category.styled';
+import { categories } from '@/assets/category';
+import { useState } from 'react';
+import BottomBtn from '@/components/common/bottombtn';
 
 const CategoryPage = () => {
+  const [selectedType, setSelectedType] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  const selectCategory = (categoryName: string) => {
+    setSelectedCategory(categoryName);
+  };
+  const handleTypeSelection = (type: string) => {
+    setSelectedType(type);
+  };
+
+  const handleSave = () => {
+    console.log('선택된 업태:', selectedType);
+    console.log('선택된 대표 카테고리:', selectedCategory);
+    //이후 api 호출 추가
+  };
+
   return (
-    <div>
+    <Container>
       <Topbar text="카테고리 설정" />
-      <div>
-        <div>업태</div>
-        <div>포장마차, 푸드트럭 선택</div>
-      </div>
-      <div>
-        <div>대표 카테고리</div>
-        <div>카테고리 리스트</div>
-      </div>
-    </div>
+      <SettingBox>
+        <TypeBox>
+          <Title>업태</Title>
+          <Select a="포장마차" b="푸드트럭" onSelect={handleTypeSelection} />
+        </TypeBox>
+        <TypeBox>
+          <Title>대표 카테고리</Title>
+          <CategoriesContainer>
+            {categories.map((category: any) => (
+              <CategoryItem key={category.id} onClick={() => selectCategory(category.name)} selected={selectedCategory === category.name}>
+                <CategoryImage src={`/images/category/${category.image}`} alt={category.name} />
+
+                <CategoryName>{category.name}</CategoryName>
+              </CategoryItem>
+            ))}
+          </CategoriesContainer>
+        </TypeBox>
+      </SettingBox>
+      <BottomBtn text="수정 하기" onClick={handleSave} />
+    </Container>
   );
 };
 
