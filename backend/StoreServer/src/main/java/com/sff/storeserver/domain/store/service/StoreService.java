4f4b07e768de2e1709455939e2f29ada46e3761a
강동윤 <yty455@naver.com>
2023-11-07 16:19:59 +0900

    Fix : 영업 종료 - 주문 서비스에 정산 요청하기

--- backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/service/StoreService.java
+++ backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/service/StoreService.java
@@ -167,6 +167,7 @@ public class StoreService {
         Store store = storeRepository.findByOwnerId(ownerId).orElseThrow(() ->
                 new BaseException(StoreError.NOT_FOUND_STORE));
 
+        // TODO - 영업 종료 후 주문 서비스에 보내서 정산 금액 받기
         store.closeBusiness();
 
     }
