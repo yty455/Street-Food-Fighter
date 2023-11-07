3e854e5eebe70bf44af579fab73307c6c3c4bc09
0214kbs <0214kbs@naver.com>
2023-11-08 00:10:52 +0900

    Feat: 커스텀 Time Picker 컴포넌트

---
+++ frontend/owner/src/components/common/timepicker/Timepicker.styled.tsx
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components';
+
+const TimePickerContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const TimePickerInput = styled.input.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const lightgray = props.theme.colors.lightgray;
+    const font = props.theme.fonts.air;
+    return css`
+      border: 1px solid ${lightgray};
+      border-radius: 5px;
+      height: 30px;
+      width: 120px;
+
+      &&::-webkit-datetime-edit {
+        text-align: center;
+        font-size: 18px;
+        font-family: ${font};
+      }
+      &:focus {
+        outline: none;
+      }
+    `;
+  }};
+`;
+
+const Label = styled.label`
+  padding-top: 6px;
+`;
+
+export { TimePickerContainer, TimePickerInput, Label };
