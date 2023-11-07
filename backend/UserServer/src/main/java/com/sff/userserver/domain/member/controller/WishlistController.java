00681c2a924813f95ea39500b59252d0f9fbe9a5
김성인 <kimsungin5@gmail.com>
2023-11-07 10:59:15 +0900

    Feat: 사장님 원해요 통계 기능 추가

--- backend/UserServer/src/main/java/com/sff/userserver/domain/member/controller/WishlistController.java
+++ backend/UserServer/src/main/java/com/sff/userserver/domain/member/controller/WishlistController.java
@@ -2,6 +2,7 @@ package com.sff.userserver.domain.member.controller;
 
 import com.sff.userserver.domain.member.dto.WishlistRequest;
 import com.sff.userserver.domain.member.dto.WishlistResponse;
+import com.sff.userserver.domain.member.dto.WishlistStatsResponse;
 import com.sff.userserver.domain.member.service.WishlistServiceImpl;
 import com.sff.userserver.global.utils.ApiResult;
 import com.sff.userserver.global.utils.ApiUtils;
@@ -32,5 +33,15 @@ public class WishlistController {
     public ApiResult<?> deleteWishlist(@RequestBody WishlistRequest wishlistRequest) {
         wishlistService.deleteWishlist(1L, wishlistRequest.getFoodType()); // TODO: 실제 인증된 회원의 ID 넣기
         return ApiUtils.success("카테고리 삭제 성공");
+    }
+
+    @GetMapping("/stats")
+    public ApiResult<?> getWishlistStats(
+            @RequestParam(name = "region1") String region1,
+            @RequestParam(name = "region2") String region2,
+            @RequestParam(name = "region3") String region3,
+            @RequestParam(name = "region4", required = false, defaultValue = "") String region4) {
+        List<WishlistStatsResponse> wishlistStats = wishlistService.getWishlistStats(region1, region2, region3, region4);
+        return ApiUtils.success(wishlistStats);
     }
 }
