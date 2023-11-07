2584742b0dc077809a4be8696cf2b06b71c0b933
0214kbs <0214kbs@naver.com>
2023-11-07 10:47:07 +0900

    Feat: 깃발 조회 mock data 추가

---
+++ frontend/owner/src/temp/flag.ts
@@ -0,0 +1,45 @@
+export const Flag1110 = [
+  {
+    flagId: 1,
+    date: '2023-11-10',
+    openTime: '10:00',
+    closeTime: '20:00',
+    address: '부산시 강서구 녹산산단 335로 7',
+    fundingAmount: 36500,
+  },
+  {
+    flagId: 2,
+    date: '2023-11-10',
+    openTime: '10:00',
+    closeTime: '20:00',
+    address: '부산시 강서구 녹산산단 335로 7',
+    fundingAmount: 30500,
+  },
+];
+
+export const Flag1111 = [
+  {
+    flagId: 1,
+    date: '2023-11-11',
+    openTime: '12:00',
+    closeTime: '20:00',
+    address: '부산시 강서구 녹산산단 335로 1',
+    fundingAmount: 36500,
+  },
+  {
+    flagId: 2,
+    date: '2023-11-11',
+    openTime: '10:00',
+    closeTime: '20:00',
+    address: '부산시 강서구 녹산산단 335로 1',
+    fundingAmount: 30500,
+  },
+  {
+    flagId: 3,
+    date: '2023-11-11',
+    openTime: '10:00',
+    closeTime: '20:00',
+    address: '부산시 강서구 녹산산단 335로 1',
+    fundingAmount: 30500,
+  },
+];
