3338b7b973fc9fa30cc36bd6b9284740b9065a64
0214kbs <0214kbs@naver.com>
2023-11-08 00:11:18 +0900

    Feat: 컴포넌트 적용

--- frontend/owner/src/components/flagset/settingbox/index.tsx
+++ frontend/owner/src/components/flagset/settingbox/index.tsx
@@ -1,11 +1,22 @@
 import useFormatDate from '@/hooks/common/formatDate.hook';
 import { Container, Content, DayContent, Title, Text } from './Settingbox.styled';
 import useSelectedDateStore from '@/stores/flag/selectedDateStore';
+import { useState } from 'react';
+import SelectTime from '../selecttime';
 
 const SettingBox = () => {
   // 선택한 날짜와 포맷
   const { selectedDate } = useSelectedDateStore();
   const formatDate = useFormatDate();
+
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+
+  const handleTimeSelection = () => {
+    setShowTimePicker(true);
+  };
+
   return (
     <Container>
       <Title>영업날짜</Title>
@@ -13,10 +24,11 @@ const SettingBox = () => {
         <Text>{formatDate(selectedDate)}</Text>
       </Content>
       <Title>영업시간</Title>
-      <DayContent>
-        <Text>영업 시간을 선택해 주세요</Text>
+      <DayContent onClick={handleTimeSelection}>
+        {startTime && endTime ? <Text>{`${startTime} ~ ${endTime}`}</Text> : <Text>영업 시간을 선택해 주세요</Text>}
         <img src="/images/common/right.png" style={{ width: '20px' }} />
       </DayContent>
+      {showTimePicker && <SelectTime onStartTimeChange={setStartTime} onEndTimeChange={setEndTime} onClose={() => setShowTimePicker(false)} />}
     </Container>
   );
 };
