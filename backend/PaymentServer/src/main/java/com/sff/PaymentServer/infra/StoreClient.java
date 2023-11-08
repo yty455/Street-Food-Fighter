951aa1333fa73e5e8136a24b22d5973db46e1287
deerKBS <deer980119@gmail.com>
2023-11-08 10:26:14 +0900

    Feat : MSA 배포 설정 추가

--- backend/PaymentServer/src/main/java/com/sff/PaymentServer/infra/StoreClient.java
+++ backend/PaymentServer/src/main/java/com/sff/PaymentServer/infra/StoreClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "storeClient", url = "${spring.data.Store_Server}")
+@FeignClient(name = "storeserver", url = "${spring.data.Store_Server}")
 public interface StoreClient {
 
     @GetMapping(value = "/api/store-service/store/{storeId}/owner")
