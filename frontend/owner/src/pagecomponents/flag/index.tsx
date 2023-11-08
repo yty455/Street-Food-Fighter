e92d8cfaaa7569faa5e9546c29886ed185af4444
0214kbs <0214kbs@naver.com>
2023-11-08 11:12:25 +0900

    Feat: 주 tab 컴포넌트 분리

--- frontend/owner/src/pagecomponents/flag/index.tsx
+++ frontend/owner/src/pagecomponents/flag/index.tsx
@@ -1,5 +1,4 @@
 import Topbar from '@/components/common/topbar';
-import Tab from '@/components/flag/tab';
 import { useState } from 'react';
 import { PageTitle, TabBox, FlagList, NoFlag } from './Flag.styled';
 import FlagCard from '@/components/flag/flagcard';
@@ -8,8 +7,7 @@ import BottomBtn from '@/components/common/bottombtn';
 import { useRouter } from 'next/navigation';
 import useSelectedDateStore from '@/stores/flag/selectedDateStore';
 import FlagDetail from '@/components/flag/flagdetail';
-
-const days = ['일', '월', '화', '수', '목', '금', '토'];
+import WeekTabs from '@/components/flag/weektabs';
 
 const FlagPage = () => {
   const { selectedDate, setSelectedDate } = useSelectedDateStore();
@@ -21,29 +19,6 @@ const FlagPage = () => {
   };
 
   const router = useRouter();
-
-  // 오늘 날짜로부터 일주일간의 날짜 계산
-  const generateWeekTabs = () => {
-    const today = new Date();
-    const tabs = [];
-
-    for (let i = 0; i < 7; i++) {
-      const currentDate = new Date(today);
-      currentDate.setDate(currentDate.getDate() + i);
-      const dayOfWeek = days[currentDate.getDay()];
-      tabs.push(
-        <Tab
-          key={i}
-          date={currentDate}
-          dayOfWeek={dayOfWeek}
-          isActive={currentDate.toDateString() === selectedDate.toDateString()}
-          onClick={selectTab}
-        />,
-      );
-    }
-
-    return tabs;
-  };
 
   // flagdetail 모달
   const [isDetailOpen, setIsDetailOpen] = useState(false);
@@ -64,7 +39,9 @@ const FlagPage = () => {
         <img src="/images/common/flag.png" style={{ width: '30px' }} />
         <div> {curflag.length} / 3 </div>
       </PageTitle>
-      <TabBox>{generateWeekTabs()}</TabBox>
+      <TabBox>
+        <WeekTabs selectedDate={selectedDate} selectTab={selectTab} />
+      </TabBox>
       {curflag.length == 0 && (
         <NoFlag>
           <div>깃발 꽂기 버튼이 </div>
