1a4f94f720c620f2cd71324dbd8578f7b6d0d55e
0214kbs <0214kbs@naver.com>
2023-11-09 10:55:33 +0900

    Feat: 깃발 카드 선택한 정보

--- frontend/owner/src/components/main/seletflag/Selectflag.styled.tsx
+++ frontend/owner/src/components/main/seletflag/Selectflag.styled.tsx
@@ -2,13 +2,13 @@ import styled, { css } from 'styled-components';
 
 const Container = styled.div.attrs<any>((props) => ({}))`
   ${(props) => {
-    const gray = props.theme.colors.lightgray;
+    const white = props.theme.colors.white;
     return css`
       display: flex;
       flex-direction: column;
       align-items: center;
 
-      background-color: ${gray};
+      background-color: ${white};
 
       width: 100vw;
       height: 100vh;
--- frontend/owner/src/components/main/seletflag/index.tsx
+++ frontend/owner/src/components/main/seletflag/index.tsx
@@ -1,18 +1,32 @@
 import Topbar from '@/components/common/topbar';
 import { Container } from './Selectflag.styled';
 import BottomBtn from '@/components/common/bottombtn';
-import FlagCard from '@/components/flag/flagcard';
+import FlagCard from '../flagcard';
+import { useState } from 'react';
 
 const SelectFlag = ({ flags, onClose, onStartOperation }: any) => {
+  const [selectedFlagId, setSelectedFlagId] = useState(null);
+
+  const handleFlagClick = (flagId: any) => {
+    setSelectedFlagId(flagId);
+  };
+
   const handleStartClick = () => {
     if (onStartOperation) {
+      console.log('Selected Flag ID: ', selectedFlagId);
       onStartOperation();
     }
   };
   return (
     <Container>
       <Topbar text="영업 시작" type="close" closeModal={onClose} />
-      <div> text</div>
+      <div>
+        <div> 깃발 선택</div>
+        <div>오늘 날짜</div>
+      </div>
+      {flags.map((flagItem: any, index: any) => (
+        <FlagCard key={index} flag={flagItem} selected={selectedFlagId === flagItem.flagId} onClick={() => handleFlagClick(flagItem.flagId)} />
+      ))}
       <BottomBtn text="영업 시작" onClick={handleStartClick} />
     </Container>
   );
