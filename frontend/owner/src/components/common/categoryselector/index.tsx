b854c2d6e7e36bb518f2973eba14053d306c0bc0
slbin <smpts00@naver.com>
2023-11-10 12:44:39 +0900

    Feat: 카테고리 타입으로 변수 저장하도록 변경

--- frontend/owner/src/components/common/categoryselector/index.tsx
+++ frontend/owner/src/components/common/categoryselector/index.tsx
@@ -5,7 +5,7 @@ const CategorySelector = ({ categories, selectedCategory, selectCategory }: any)
   return (
     <CategoriesContainer>
       {categories.map((category: any) => (
-        <CategoryItem key={category.id} onClick={() => selectCategory(category.name)} selected={selectedCategory === category.name}>
+        <CategoryItem key={category.id} onClick={() => selectCategory(category.type)} selected={selectedCategory === category.type}>
           <CategoryImage src={`/images/category/${category.image}`} alt={category.name} />
           <CategoryName>{category.name}</CategoryName>
         </CategoryItem>
