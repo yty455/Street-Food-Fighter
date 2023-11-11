28673c3a5c73fdf3c9f058d02400d112fd448b67
slbin <smpts00@naver.com>
2023-11-12 00:56:12 +0900

    Feat: UserId 헤더에서 추출하도록 변경

--- backend/StoreServer/src/main/java/com/sff/storeserver/domain/flag/controller/FlagController.java
+++ backend/StoreServer/src/main/java/com/sff/storeserver/domain/flag/controller/FlagController.java
@@ -1,5 +1,7 @@
 package com.sff.storeserver.domain.flag.controller;
 
+import com.sff.storeserver.common.aop.UserIdHolder;
+import com.sff.storeserver.common.aop.UserIdRequired;
 import com.sff.storeserver.common.utils.ApiResult;
 import com.sff.storeserver.common.utils.ApiUtils;
 import com.sff.storeserver.domain.flag.dto.FlagDetailResponse;
@@ -23,23 +25,25 @@ public class FlagController {
 
     @Operation(summary = "사장 - 깃발 등록", description = "사장이 지정한 위치에 깃발을 등록 합니다.")
     @PostMapping("/flags")
-    public ApiResult<?> registerFlag(@Valid @RequestBody FlagRequest flagRequest) {
-        Long flagId = flagService.createFlag(flagRequest);
+    @UserIdRequired
+    public ApiResult<?> registerFlag(UserIdHolder userIdHolder, @Valid @RequestBody FlagRequest flagRequest) {
+        Long flagId = flagService.createFlag(userIdHolder.getUserId(), flagRequest);
         return ApiUtils.success(flagId);
     }
 
     @Operation(summary = "사장 - 깃발 조회", description = "사장이 선택한 날짜의 깃발을 조회 합니다..")
-    @GetMapping("/store/{storeId}/flags")
-    public ApiResult<?> getFlags(@PathVariable("storeId") Long storeId,
-                                 @RequestParam @Parameter(name = "date", description = "조회할 날짜") LocalDate date) {
-        List<FlagResponse> flagResponses = flagService.getFlags(storeId, date);
+    @GetMapping("/store/flags")
+    @UserIdRequired
+    public ApiResult<?> getFlags(UserIdHolder userIdHolder, @RequestParam @Parameter(name = "date", description = "조회할 날짜") LocalDate date) {
+        List<FlagResponse> flagResponses = flagService.getFlags(userIdHolder.getUserId(), date);
         return ApiUtils.success(flagResponses);
     }
 
     @Operation(summary = "사장 - 깃발 상세 조회", description = "깃발의 상세 정보를 조회 합니다.")
     @GetMapping("/store/{storeId}/flags/{flagId}")
-    public ApiResult<?> getFlagDetail(@PathVariable("storeId") Long storeId, @PathVariable("flagId") Long flagId) {
-        FlagDetailResponse flagDetailResponse = flagService.getFlagDetail(storeId, flagId);
+    @UserIdRequired
+    public ApiResult<?> getFlagDetail(UserIdHolder userIdHolder, @PathVariable("flagId") Long flagId) {
+        FlagDetailResponse flagDetailResponse = flagService.getFlagDetail(userIdHolder.getUserId(), flagId);
         return ApiUtils.success(flagDetailResponse);
     }
 }
