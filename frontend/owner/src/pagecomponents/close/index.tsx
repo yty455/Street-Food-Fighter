7f008124574df74bb0c00fc1a5da3ca57d5a3a3c
0214kbs <0214kbs@naver.com>
2023-11-13 01:26:03 +0900

    Feat: 정산 api 연결 및 hook 분리

--- frontend/owner/src/pagecomponents/close/index.tsx
+++ frontend/owner/src/pagecomponents/close/index.tsx
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react';
 import DonutChart from '@/components/close/donutchart';
 import Topbar from '@/components/common/topbar';
 import { Content, Text } from './Close.styled';
 import BottomBtn from '@/components/common/bottombtn';
 import Receipt from '@/components/common/receipt';
 import { useRouter } from 'next/navigation';
-import StatsAPI from '@/apis/close/StatsAPI';
-import { AccountStats } from '@/types/stats.type';
+import useStatsHook from '@/hooks/apis/stats.hook';
 
 const ClosePage = () => {
   const router = useRouter();
-  const [accounts, setAccounts] = useState<AccountStats>({
-    menuStatsList: [],
-    totalPrice: 0,
-  });
-
-  useEffect(() => {
-    const fetchStats = async () => {
-      const data = await StatsAPI();
-      setAccounts(data);
-    };
-    fetchStats();
-  }, []);
+  const accounts = useStatsHook();
 
   return (
     <div>
@@ -29,9 +16,9 @@ const ClosePage = () => {
       <Content>
         <Text>수고하셨습니다! </Text>
         <DonutChart />
-        {/* <div style={{ width: '90vw' }}>
+        <div style={{ width: '90vw' }}>
           <Receipt orderItemList={accounts.menuStatsList} totalPrice={accounts.totalPrice} type="total" />
-        </div> */}
+        </div>
       </Content>
       <BottomBtn
         text="홈으로"
