3d6a5440c976558cc6343d80dbe18097363fc7a9
0214kbs <0214kbs@naver.com>
2023-11-12 23:23:52 +0900

    Feat: accesstoken 적용

--- frontend/owner/src/apis/close/StatsAPI.ts
+++ frontend/owner/src/apis/close/StatsAPI.ts
@@ -1,8 +1,7 @@
 import api from '../api';
 
 const StatsAPI = async () => {
-  // 임시
-  const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+  const accessToken = localStorage.getItem('accessToken');
 
   try {
     const response = await api.get(`/api/order-server/orders/stats`, {
