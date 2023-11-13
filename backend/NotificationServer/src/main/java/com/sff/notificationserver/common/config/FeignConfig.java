90a2b2a0c4cd92b1c44894ece620bcba53703f98
강동윤 <yty455@naver.com>
2023-11-13 10:36:46 +0900

    Fix : Notification - FeignConfig

--- backend/NotificationServer/src/main/java/com/sff/notificationserver/common/config/FeignConfig.java
+++ backend/NotificationServer/src/main/java/com/sff/notificationserver/common/config/FeignConfig.java
@@ -1,6 +1,5 @@
 package com.sff.notificationserver.common.config;
 
-import feign.Logger;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
@@ -11,19 +10,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Configuration
 public class FeignConfig {
     @Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
-    }
-
-//    @Bean
-//    public RequestInterceptor requestInterceptor() {
-//        return requestTemplate -> {
-//            requestTemplate.header("Content-Type", "application/json");
-//            requestTemplate.header("Accept", "application/json");
-//        };
-//    }
-
-    @Bean
     public RequestInterceptor requestInterceptor() {
         return new RequestInterceptor() {
             @Override
@@ -31,18 +17,16 @@ public class FeignConfig {
                 ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
                         .getRequestAttributes();
 
-                template.header("Content-Type", "application/json");
-                template.header("Accept", "application/json");
-
                 if (attributes == null) {
                     return;
                 }
 
                 String userId = attributes.getRequest().getHeader("UserId");
-                // jwt 토큰도 동일하게 추가 필요
                 if (userId != null) {
                     template.header("UserId", userId);
                 }
+
+
             }
         };
     }
