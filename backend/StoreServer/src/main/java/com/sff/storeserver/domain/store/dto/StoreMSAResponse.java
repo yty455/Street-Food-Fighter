388fce8fcae9ab60bb03756a6627e9944b9a014d
강동윤 <yty455@naver.com>
2023-11-13 09:58:24 +0900

    Fix - StoreMSAResponse

--- backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/dto/StoreMSAResponse.java
+++ backend/StoreServer/src/main/java/com/sff/storeserver/domain/store/dto/StoreMSAResponse.java
@@ -1,5 +1,6 @@
 package com.sff.storeserver.domain.store.dto;
 
+import com.sff.storeserver.domain.store.entity.CategoryType;
 import com.sff.storeserver.domain.store.entity.Store;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,11 +15,13 @@ public class StoreMSAResponse {
     private Long storeId;
     private String name;
     private String activeArea;
+    private CategoryType categoryType;
 
     public static StoreMSAResponse fromEntity(Store store) {
         return StoreMSAResponse.builder()
                 .storeId(store.getId())
                 .name(store.getName())
-                .activeArea(store.getActiveArea()).build();
+                .activeArea(store.getActiveArea())
+                .categoryType(store.getCategory()).build();
     }
 }
