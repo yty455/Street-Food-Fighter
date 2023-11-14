b110a0fc1fe0601c4fa624e8e299153940218e46
0214kbs <0214kbs@naver.com>
2023-11-14 09:37:05 +0900

    Feat: 깃발조회 기능 추가

--- frontend/owner/src/pagecomponents/flag/index.tsx
+++ frontend/owner/src/pagecomponents/flag/index.tsx
@@ -9,14 +9,20 @@ import useSelectedDateStore from '@/stores/flag/selectedDateStore';
 import FlagDetail from '@/components/flag/flagdetail';
 import WeekTabs from '@/components/flag/weektabs';
 import useModal from '@/hooks/common/modal.hook';
+import DateFlagAPI from '@/apis/flag/DateFlagAPI';
 
 const FlagPage = () => {
   const { selectedDate, setSelectedDate } = useSelectedDateStore();
-  const curflag = Flag2;
+  const [curflag, setCurFlag] = useState([]);
 
-  const selectTab = (date: any) => {
+  const selectTab = async (date: any) => {
     setSelectedDate(date);
-    // 선택된 요일에 따라 API 호출
+    const formattedDate = new Date(date).toISOString().split('T')[0]; // 날짜 포맷 변경
+    const res = await DateFlagAPI(formattedDate);
+    const flags = res.response;
+    if (flags) {
+      setCurFlag(flags);
+    }
   };
 
   const router = useRouter();
