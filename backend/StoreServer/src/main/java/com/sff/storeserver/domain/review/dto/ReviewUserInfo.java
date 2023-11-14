59a1539813e828b5b953c42faf74363b74dfd328
강동윤 <yty455@naver.com>
2023-11-14 17:48:56 +0900

    Fix : 가게 리뷰 조회

--- backend/StoreServer/src/main/java/com/sff/storeserver/domain/review/dto/ReviewUserInfo.java
+++ backend/StoreServer/src/main/java/com/sff/storeserver/domain/review/dto/ReviewUserInfo.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewUserInfo {
-
+    private Long userId;
     private String nickname;
     private String imageUrl;
 
