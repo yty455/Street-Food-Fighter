6d700476d509c1a47543d1a1ee6481bc99383b9e
0214kbs <0214kbs@naver.com>
2023-11-15 02:49:57 +0900

    Feat: 충전하기 api 연동

--- frontend/customer/src/components/common/charge/index.tsx
+++ frontend/customer/src/components/common/charge/index.tsx
@@ -3,14 +3,26 @@ import Input from '@/components/common/input';
 import BottomBtn from '@/components/common/bottombtn';
 import { Topbar, ContentBox, ModalOverlay, TopContent, Title, StyledButton, SuccessContentBox, FinishText } from './Charge.styled';
 import RoundButton from '../roundbtn';
+import ChargeAPI from '@/apis/user/ChargeAPI';
 
 export const Charge = ({ toggleCharge }: any) => {
   const [change, setChange] = useState('');
-  const [success, setSuccess] = useState(true);
+  const [success, setSuccess] = useState(false);
   const handleChange = (e: any) => {
     setChange(e.target.value);
   };
 
+  const handleToCharge = async () => {
+    const data = {
+      amount: parseInt(change),
+      isCharge: true,
+      paymentPassword: '',
+    };
+    const res = await ChargeAPI({ data });
+    if (res) {
+      setSuccess(true);
+    }
+  };
   return (
     <ModalOverlay>
       <div>
@@ -29,16 +41,16 @@ export const Charge = ({ toggleCharge }: any) => {
               </Title>
               <Input value={change} onChange={handleChange} placeholder="충전할 금액을 입력해주세요" maxLength={25} use="info" />
             </ContentBox>
-            <BottomBtn text="충전 하기" />
+            <BottomBtn text="충전 하기" onClick={handleToCharge} />
           </div>
         ) : (
           <SuccessContentBox>
             <img src="/images/signup/finish.png" style={{ width: '130px' }} />
             <div>
-              <FinishText>100,000 파이트 머니가</FinishText>
+              <FinishText>{Number(change).toLocaleString()} 파이트 머니가</FinishText>
               <FinishText> 충전이 완료되었습니다.</FinishText>
             </div>
-            <StyledButton>
+            <StyledButton onClick={toggleCharge}>
               <RoundButton text="돌아가기" font="18px" />
             </StyledButton>
           </SuccessContentBox>
