5b608aebab7f89852007c0c109c70c3f113eddec
slbin <smpts00@naver.com>
2023-11-16 01:55:18 +0900

    Feat: 데이터가 있을시 보이도록 수정

--- frontend/customer/src/components/userinfo/infobox/index.tsx
+++ frontend/customer/src/components/userinfo/infobox/index.tsx
@@ -10,44 +10,48 @@ const InfoBox = ({ onEditClick }: any) => {
 
   return (
     <BoxContainer>
-      <ContentBox onClick={() => onEditClick('email')}>
-        <InfoTitle>이메일</InfoTitle>
-        <Right>
-          <Content>{userInfo.email}</Content>
-          <img src="/images/common/right.png" style={{ height: '20px' }} />
-        </Right>
-      </ContentBox>
-      <ContentBox onClick={() => onEditClick('nickname')}>
-        <InfoTitle>닉네임</InfoTitle>
-        <Right>
-          <Content>{userInfo.nickname}</Content>
-          <img src="/images/common/right.png" style={{ height: '20px' }} />
-        </Right>
-      </ContentBox>
-      <ContentBox onClick={() => onEditClick('phone')}>
-        <InfoTitle>핸드폰 번호</InfoTitle>
-        <Right>
-          <Content>{userInfo.phone}</Content>
-          <img src="/images/common/right.png" style={{ height: '20px' }} />
-        </Right>
-      </ContentBox>
-      <ContentBox onClick={() => onEditClick('region')}>
-        <InfoTitle>내 동네</InfoTitle>
-        <Right>
-          <Content>
-            {userInfo.region1} {userInfo.region2} {userInfo.region3} {userInfo.region4}
-          </Content>
-          <img src="/images/common/right.png" style={{ height: '20px' }} />
-        </Right>
-      </ContentBox>
-      <ContentBox
-        onClick={() => {
-          router.push('/password/change');
-        }}
-      >
-        <InfoTitle>결제 비밀번호 변경</InfoTitle>
-        <img src="/images/common/right.png" style={{ height: '20px' }} />
-      </ContentBox>
+      {userInfo && (
+        <>
+          <ContentBox onClick={() => onEditClick('email')}>
+            <InfoTitle>이메일</InfoTitle>
+            <Right>
+              <Content>{userInfo.email}</Content>
+              <img src="/images/common/right.png" style={{ height: '20px' }} />
+            </Right>
+          </ContentBox>
+          <ContentBox onClick={() => onEditClick('nickname')}>
+            <InfoTitle>닉네임</InfoTitle>
+            <Right>
+              <Content>{userInfo.nickname}</Content>
+              <img src="/images/common/right.png" style={{ height: '20px' }} />
+            </Right>
+          </ContentBox>
+          <ContentBox onClick={() => onEditClick('phone')}>
+            <InfoTitle>핸드폰 번호</InfoTitle>
+            <Right>
+              <Content>{userInfo.phone}</Content>
+              <img src="/images/common/right.png" style={{ height: '20px' }} />
+            </Right>
+          </ContentBox>
+          <ContentBox onClick={() => onEditClick('region')}>
+            <InfoTitle>내 동네</InfoTitle>
+            <Right>
+              <Content>
+                {userInfo.region1} {userInfo.region2} {userInfo.region3} {userInfo.region4}
+              </Content>
+              <img src="/images/common/right.png" style={{ height: '20px' }} />
+            </Right>
+          </ContentBox>
+          <ContentBox
+            onClick={() => {
+              router.push('/password/change');
+            }}
+          >
+            <InfoTitle>결제 비밀번호 변경</InfoTitle>
+            <img src="/images/common/right.png" style={{ height: '20px' }} />
+          </ContentBox>
+        </>
+      )}
     </BoxContainer>
   );
 };
