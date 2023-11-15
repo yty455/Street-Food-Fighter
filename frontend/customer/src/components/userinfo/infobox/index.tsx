06c10db3300eae4becff2d8bf0a8c0f664c64463
slbin <smpts00@naver.com>
2023-11-16 01:53:41 +0900

    Feat: 마이페이지 유저 정보 바인딩

--- frontend/customer/src/components/userinfo/infobox/index.tsx
+++ frontend/customer/src/components/userinfo/infobox/index.tsx
@@ -1,30 +1,33 @@
 import { user } from '@/temp/user';
 import { BoxContainer, Content, ContentBox, InfoTitle, Right } from './Userinfo.styled';
 import { useRouter } from 'next/navigation';
+import userInfoStore from '@/stores/userInfoStore';
 
 const InfoBox = ({ onEditClick }: any) => {
   const curuser = user;
   const router = useRouter();
+  const userInfo = userInfoStore();
+
   return (
     <BoxContainer>
       <ContentBox onClick={() => onEditClick('email')}>
         <InfoTitle>이메일</InfoTitle>
         <Right>
-          <Content>{curuser.email}</Content>
+          <Content>{userInfo.email}</Content>
           <img src="/images/common/right.png" style={{ height: '20px' }} />
         </Right>
       </ContentBox>
       <ContentBox onClick={() => onEditClick('nickname')}>
         <InfoTitle>닉네임</InfoTitle>
         <Right>
-          <Content>{curuser.nickname}</Content>
+          <Content>{userInfo.nickname}</Content>
           <img src="/images/common/right.png" style={{ height: '20px' }} />
         </Right>
       </ContentBox>
       <ContentBox onClick={() => onEditClick('phone')}>
         <InfoTitle>핸드폰 번호</InfoTitle>
         <Right>
-          <Content>{curuser.phone}</Content>
+          <Content>{userInfo.phone}</Content>
           <img src="/images/common/right.png" style={{ height: '20px' }} />
         </Right>
       </ContentBox>
@@ -32,7 +35,7 @@ const InfoBox = ({ onEditClick }: any) => {
         <InfoTitle>내 동네</InfoTitle>
         <Right>
           <Content>
-            {curuser.region1} {curuser.region2} {curuser.region3} {curuser.region4}
+            {userInfo.region1} {userInfo.region2} {userInfo.region3} {userInfo.region4}
           </Content>
           <img src="/images/common/right.png" style={{ height: '20px' }} />
         </Right>
