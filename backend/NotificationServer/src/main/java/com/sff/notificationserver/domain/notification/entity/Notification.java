d3ac4109ad7eefe7926e7d4d0534c9d099d8d465
강동윤 <yty455@naver.com>
2023-11-16 01:07:43 +0900

    Fix : Noti - @EntityListeners 추가

--- backend/NotificationServer/src/main/java/com/sff/notificationserver/domain/notification/entity/Notification.java
+++ backend/NotificationServer/src/main/java/com/sff/notificationserver/domain/notification/entity/Notification.java
@@ -3,6 +3,7 @@ package com.sff.notificationserver.domain.notification.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(toBuilder = true)
+@EntityListeners(AuditingEntityListener.class)
 public class Notification {
 
     @Id
